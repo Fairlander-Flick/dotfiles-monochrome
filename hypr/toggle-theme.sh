64b7a5dce8321fda4c4d1b84eb73bdbc3d3e6f50
Ugur Tasdan <flick.tasdan@fau.de>
2026-05-10 22:30:44 +0200

    fix: smooth 1s fade transition for theme toggle and fix wallpaper paths

--- hypr/toggle-theme.sh
+++ hypr/toggle-theme.sh
@@ -16,13 +16,13 @@ CURRENT_THEME=$(cat "$STATE_FILE")
 
 if [ "$CURRENT_THEME" == "dark" ]; then
     # Switch to LIGHT
-    awww img "$WALL_LIGHT" --transition-type fade --transition-duration 1 --filter Nearest
+    awww img "$WALL_LIGHT" --transition-type fade --transition-duration 1
     ln -sf "$WAYBAR_CONFIG/style-light.css" "$WAYBAR_CONFIG/style.css"
     gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
     echo "light" > "$STATE_FILE"
 else
     # Switch to DARK
-    awww img "$WALL_DARK" --transition-type fade --transition-duration 1 --filter Nearest
+    awww img "$WALL_DARK" --transition-type fade --transition-duration 1
     ln -sf "$WAYBAR_CONFIG/style-dark.css" "$WAYBAR_CONFIG/style.css"
     gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
     echo "dark" > "$STATE_FILE"
