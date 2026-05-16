119d36b0d4dd1e639f9c182d2cfd5564d59dc3bf
Ugur Tasdan <flick.tasdan@fau.de>
2026-05-16 21:55:38 +0200

    Update: 3-stage theme toggle system and gray style

--- hypr/toggle-theme.sh
+++ hypr/toggle-theme.sh
@@ -1,32 +1,43 @@
 #!/bin/bash
 
-# Theme Toggle Script
+# Theme Toggle Script (3 Stages: Light, Gray, Dark)
 STATE_FILE="$HOME/.cache/theme_state"
-WALL_DARK="$HOME/Wallpapers/black.jpg"
-WALL_LIGHT="$HOME/Wallpapers/white.jpg"
+WALL_WHITE="$HOME/Wallpapers/white.jpg"
+WALL_BLACK="$HOME/Wallpapers/black.jpg"
+WALL_BLACK1="$HOME/Wallpapers/black1.jpg"
 
 WAYBAR_CONFIG="$HOME/.config/waybar"
 
-# Create state file if it doesn't exist
+# Create state file if it doesn't exist (0: light, 1: gray, 2: dark)
 if [ ! -f "$STATE_FILE" ]; then
-    echo "dark" > "$STATE_FILE"
+    echo "0" > "$STATE_FILE"
 fi
 
-CURRENT_THEME=$(cat "$STATE_FILE")
+STATE=$(cat "$STATE_FILE")
 
-if [ "$CURRENT_THEME" == "dark" ]; then
-    # Switch to LIGHT
-    awww img "$WALL_LIGHT" --transition-type fade --transition-duration 1
-    ln -sf "$WAYBAR_CONFIG/style-light.css" "$WAYBAR_CONFIG/style.css"
-    gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
-    echo "light" > "$STATE_FILE"
-else
-    # Switch to DARK
-    awww img "$WALL_DARK" --transition-type fade --transition-duration 1
-    ln -sf "$WAYBAR_CONFIG/style-dark.css" "$WAYBAR_CONFIG/style.css"
-    gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-    echo "dark" > "$STATE_FILE"
-fi
+case $STATE in
+    0)
+        # Switch to GRAY (State 1)
+        awww img "$WALL_BLACK" --transition-type fade --transition-duration 1
+        ln -sf "$WAYBAR_CONFIG/style-gray.css" "$WAYBAR_CONFIG/style.css"
+        gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+        echo "1" > "$STATE_FILE"
+        ;;
+    1)
+        # Switch to DARK (State 2)
+        awww img "$WALL_BLACK1" --transition-type fade --transition-duration 1
+        ln -sf "$WAYBAR_CONFIG/style-dark.css" "$WAYBAR_CONFIG/style.css"
+        gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+        echo "2" > "$STATE_FILE"
+        ;;
+    *)
+        # Switch to LIGHT (State 0)
+        awww img "$WALL_WHITE" --transition-type fade --transition-duration 1
+        ln -sf "$WAYBAR_CONFIG/style-light.css" "$WAYBAR_CONFIG/style.css"
+        gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
+        echo "0" > "$STATE_FILE"
+        ;;
+esac
 
 # Reload Waybar
 pkill -USR2 waybar
